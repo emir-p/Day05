51aaacbf902648cb21db1826bc5369b0e70f55e3
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-13 00:24:55 +0100

    Last Character in a String

---
+++ ex12/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+	// Only change code below this line
+	var lastName = "Tesla";
+	var lastLetter0fLastName = lastName[lastName.length - 1];
+	
+	// Only change code above this line
+	return lastLetter0fLastName;
+}
+
+console.log(main());
+module.exports = main;
