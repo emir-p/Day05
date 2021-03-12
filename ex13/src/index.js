2957aac594ac7b0f5af68e3900e9db34255d4552
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-13 00:32:58 +0100

    Nth-to-Last Character

---
+++ ex13/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+	// Only change code below this line
+	var lastName = "Bezos";
+	var thirdToLastLetter0fLastName = lastName[lastName.length - 3];
+	
+	// Only change code above this line
+	return thirdToLastLetter0fLastName;
+}
+
+console.log(main());
+module.exports = main;
