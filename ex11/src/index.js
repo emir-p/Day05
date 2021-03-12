e16be65cf6751b735f1ff31f9cae19d8a49ff26f
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-13 00:20:44 +0100

    Find the Nth Character in a String

---
+++ ex11/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+	// Only change code below this line
+	var lastName = "Tesla";
+	var thirdLetter0fLastName = lastName[2];
+	
+	// Only change code above this line
+	return thirdLetter0fLastName;
+}
+
+console.log(main());
+module.exports = main;
